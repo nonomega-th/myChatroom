8579315af49150b62e8503230633983067972f35
Timanon Losupanphorn <nonomega@gmail.com>
2018-09-05 15:07:26 +0700

    New UI

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var socketio = require('socket.io')(http);
+var server = require('http').Server(app);
+var socketio = require('socket.io')(server);
 var port = process.env.PORT || 5000;
+users = [];
+connections = [];
 
+server.listen(port, function(){
+  console.log('listening on *:'+port);
+});
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 socketio.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chatpipe', function(msg){
-    console.log('message: ' + msg);
-    socketio.emit('chatpipe', msg);
-  });
+	connections.push(socket);
+	console.log('Connection: %s sockets connected.',connections.length);
+
+	// Disconnect
+	socket.on('disconnect', function(data){
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected.', connections.length);
+	});
+	
+	// Send Message
+	socket.on('chatpipe', function(msg){
+	    console.log('message: ' + msg);
+	    socketio.emit('newMessage', msg);
+	});
 });
 
-http.listen(port, function(){
-  console.log('listening on *:'+port);
-});

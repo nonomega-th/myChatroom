5c4bd72d555ad94b1a96d4fd1a82beb05776d0aa
Timanon Losupanphorn <nonomega@gmail.com>
2018-09-06 17:33:44 +0700

    Edit Chat room UI
    Edit chat room UI
    clean some code

--- public/script.js
+++ public/script.js
@@ -11,6 +11,7 @@ $(function () {
 
   var users = $('#users');
   var username = $('#username');
+  var me;
 
   message.keypress(function (event) {
     if(event.which == 13 && !event.shiftKey) {        
@@ -28,7 +29,14 @@ $(function () {
     event.preventDefault();
   });
   socket.on('newMessage', function(payload){
-      chat.append('<div class="well"><strong>' +payload.user+ ': </strong>' + payload.message + '</div>');
+    var element = document.getElementById("chat");
+    element.scrollTop = element.scrollHeight;
+    if (payload.user == me) {
+      chat.append(genmybubble(payload));
+    }else{
+      chat.append(genyourbubble(payload));
+    }
+    // chat.append('<p class="bubble you"><strong>' +payload.user+ ': </strong>' + payload.message + '</p>');
   });
 
   userForm.submit(function(event){
@@ -37,20 +45,28 @@ $(function () {
     if ($.trim(usernametemp) != '') {
       socket.emit('newUser', usernametemp, function(data){
         if(data){
+          me = usernametemp;
           userFormArea.hide();
           messageFormArea.show();
+          messagesinput.focus();
         }
       });
       username.val('');
     }
-    $('#messagesinput').val('');
+    $('#username').val('');
   });
 
   socket.on('getUsers', function(userlist){
     var html = '';
     for(i=0; i<userlist.length; i++){
-      html += '<div class="list-group-item" style="margin-top: 5px">' + userlist[i] + '</div>';
+      html += '<div class="list-group-item" style="margin-top:5px">' + userlist[i] + '</div>';
     } 
     users.html(html);
   });
 });
+function genmybubble(payload){
+  return '<li class="bubble me">'+ payload.message + '</li>';
+}
+function genyourbubble(payload){
+  return '<li class="yourusername"><strong>' +payload.user+ '</strong></li><li class="bubble you">' + payload.message + '</li>';
+}
